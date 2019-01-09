e9b9da9fa7ad00cc9b3cd437d38e758b2bee3e1e
Pedro Maglione <pedromslp@gmail.com>
2019-01-09 12:01:48 +0100

    Evaluator fn receives all votes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import math
 import numpy as np
 import random
 from scipy.stats import beta
+import collections
 
 #utils
 def binomial_likelihood(p, n, y):
@@ -23,25 +24,46 @@ class Evaluator:
     '''
     Function for deciding to continue or not collecting votes over a task.
 
-    votes - array of votes
-    classification_threshold - value between 0 and 1 for deciding if prob of data is enough or must continue
-    cost_ratio - ratio of crowd to expert cost, value between 0 and 1
-    classification_function - function to calculate how likely is to be classified
-    accuracy - crowd accuracy, value between 0 and 1
-    prior - prior believe about the item satisfies a condition, .5 is total uncertainty
+    Input:
+        votes - dictionary of dictionaries, containing the votes over each item where keys corresponds to workers ID
+            {
+                item_i: {worker_i:vote...worker_n:vote},
+                ...
+                item_n: {worker_i:vote...worker_n:vote},
+            }
+        classification_threshold - value between 0 and 1 for deciding if prob of data is enough or must continue
+        cost_ratio - ratio of crowd to expert cost, value between 0 and 1
+        classification_function - function to calculate how likely is to be classified
+        
+    Output:
+        Dictionary with the decision for each item (True/False), indexed by item_id
+            {
+                item_id: Boolean
+                ...
+                item_n: Boolean
+            }
     '''
-    def decision_fn(votes, classification_threshold, cost_ratio, classification_function, accuracy):
+    def decision_fn(votes, classification_threshold, cost_ratio, classification_function):
+        print(votes)
+        
         prior = .5
-        posterior = classification_function(votes, prior, accuracy)
-        #next_prob = accuracy * posterior + (1 - accuracy) * (1 - posterior)
-
-        if posterior > classification_threshold:
-            return False
-        elif(len(votes) >= (1 / cost_ratio)):
-            return False
-        else:
-            return True
+        accuracy = .75 #must be computed using the votes, EM or DawidSkene
+        
+        results = dict.fromkeys(range(items_num), True)
+        
+        for item_id, item_votes in votes.items():
+            posterior = classification_function(item_votes, prior, accuracy)
+            #next_prob = accuracy * posterior + (1 - accuracy) * (1 - posterior)
     
+            if posterior > classification_threshold:
+                results[item_id] = True
+            elif(len(votes) >= (1 / cost_ratio)):
+                results[item_id] = False
+            else:
+                results[item_id] = True
+                
+        return results
+        
     
 class Workers:
 
@@ -68,7 +90,7 @@ class Workers:
 class Classificator:
     def classification_fn_posterior(votes, prior, accuracy):    
         n = len(votes)
-        y = sum(votes)
+        y = sum(votes.values())
 
         likelihood = binomial_likelihood(prior, n, y)
 
@@ -79,7 +101,7 @@ class Classificator:
     
     def classification_fn_beta_pdf(votes, th, accuracy):    
         n = len(votes)
-        y = sum(votes)
+        y = sum(votes.values())
 
         posterior = beta.sf(th, 1 + y, 1 + (n - y))
 
@@ -107,8 +129,10 @@ class Generator:
         return gold_data
     
     def get_random_worker_accuracy(self, item, items_num):       
-        worker_found = False
         '''
+        #TO-DO: add logic to avoid worker vote on same task
+        worker_found = False
+        
         while (worker_found == False):
             index = np.random.randn(0, self.workers_num - 1)
 
@@ -116,47 +140,50 @@ class Generator:
                 self.index_workers_voted_on_item[item].append(index)
                 worker_found = True
         ''' 
-                
-        return self.workers_accuracy[random.randint(0, self.workers_num - 1)]
+        worker_id = random.randint(0, self.workers_num - 1)
+        return (worker_id, self.workers_accuracy[worker_id])
     
     def generate_votes_gt(self, items_num):
-        workers_accuracy = self.workers_accuracy
-        total_votes = []
-        workers_num = len(self.workers_accuracy)
-        accuracy_media = get_workers_accuracy(workers_accuracy)
-
+        #workers_accuracy = self.workers_accuracy
+        total_votes = collections.defaultdict(dict)
+        #workers_num = len(self.workers_accuracy)
+        #accuracy_media = get_workers_accuracy(workers_accuracy)
+        results = dict.fromkeys(range(items_num), True) #Must collect votes for all items
+        
+        #Every iteration check all items, performance can be improved
         for i in range(items_num):
-            item_votes = []
             get_more_votes = True
             while(get_more_votes):
-                worker_acc = self.get_random_worker_accuracy(i, items_num)
-               
-                if np.random.binomial(1, worker_acc):
-                    vote = 1
-                else:
-                    vote = 0
-                item_votes.append(vote)
-                
-                #Ask if must continue or not
-                get_more_votes = Evaluator.decision_fn(item_votes, self.classification_threshold, self.cost_ratio, 
-                                                           Classificator.classification_fn_posterior, accuracy_media)
+                if (results[i]): #check if must continue collecting votes for this item
+                    worker_id, worker_acc = self.get_random_worker_accuracy(i, items_num)
+                   
+                    if np.random.binomial(1, worker_acc):
+                        vote = 1
+                    else:
+                        vote = 0
+                        
+                    total_votes[i][worker_id] = vote
+                    
+                    #Ask if must continue or not
+                    results = Evaluator.decision_fn(total_votes, self.classification_threshold, self.cost_ratio, 
+                                                               Classificator.classification_fn_posterior)
+                    #Stop when decided stop for all items, all items = False
+                    get_more_votes = sum([x for x in results if x == False]) == items_num
             #end while
-                
-            total_votes.append(item_votes)
          #end for     
             
         return total_votes
 
 
 
-#assumptions
+#Assumptions
 #1 condition
 #difficulty of tasks are all equal
 #there are no test questions
 #there are a percent of cheaters
         
 z = 0.1 #% cheaters
-items_num = 100
+items_num = 5
 ct = .9
 cr = .01 #ratio 1:100
 iter_num = 1
@@ -177,9 +204,9 @@ for _ in range(iter_num):
     
     votes = Generator(params).generate_votes_gt(items_num)
 #end for
-
+'''
 print("#items classified: {}".format(len([a for a in votes if len(a) < (1/cr)])))
 print("Workers general acc: %{:1.2f}".format(get_workers_accuracy(workers_accuracy) * 100))
 print("# ground truth IN items: {}".format(len([a for a in ground_truth if a == 1])))
-
+'''
 #print(sum([sum(a) for a in votes])/len(votes))
