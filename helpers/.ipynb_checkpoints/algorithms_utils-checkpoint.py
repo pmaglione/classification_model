77a06fa5a1c494ce7a8f499f36dd6591bcad718b
Pedro Maglione <pedromslp@gmail.com>
2019-02-23 13:36:21 +0100

    Clean and add real world management

--- helpers/.ipynb_checkpoints/algorithms_utils-checkpoint.py
+++ helpers/.ipynb_checkpoints/algorithms_utils-checkpoint.py
@@ -323,3 +323,53 @@ def get_dataset_next_worker_vote(responses, item_votes, i, categories):
             break
                 
     raise ValueError('empty value')
+    
+'''    
+    Input: folder name of real world dataset, must have SQUARE file structure
+    Output: folder, ground truth values,  workers accuracies
+'''
+def get_real_dataset_data(folder):
+    path = f'data/{folder}/workerResponses.txt'
+    votes = pd.read_csv(path, delimiter='\t', header=None)
+    path = f'data/{folder}/groundTruth.txt'
+    ground_truth = pd.read_csv(path, delimiter='\t', header=None)
+    path = f'data/{folder}/categories.txt'
+    categories = pd.read_csv(path, delimiter='\t', header=None)
+    
+    '''
+        1. Get workers ids
+        2. Iterate over worker responses and compare with ground truth
+        3. Calculate worker accuracy 
+    '''
+    
+    w_id_int = 0
+    workers_accuracy = {}
+    for worker_id in np.unique(votes[0]):
+        
+        worker_votes = votes[votes[0] == worker_id]
+        worker_successes = 0
+        worker_tasks = 0
+               
+        for index, worker_vote_info in worker_votes.iterrows():
+            
+            worker_vote_task = worker_vote_info[1]
+            worker_vote = worker_vote_info[2]
+            
+            gt_val = ground_truth[ground_truth[0]==worker_vote_task][1]
+            
+            if len(gt_val) > 0:
+                worker_tasks += 1
+                if worker_vote == gt_val.item():
+                    worker_successes += 1
+        #endfor
+        
+        if worker_tasks > 0:
+            w_acc = worker_successes / worker_tasks 
+            workers_accuracy[w_id_int] = [w_acc,w_acc] #workaround to be equal as generated positive/negative accs structure
+            w_id_int += 1
+            
+      
+    classes_normalized = [x for x in categories[0]] 
+    gt_normalized = [classes_normalized.index(x) for x in ground_truth[1]] #map categories to 0/1 by index
+    
+    return gt_normalized, workers_accuracy
